a7069b51066ee38c26d9977b0d51ddd8fc352bed
certlegendary <whitestar0713@gmail.com>
2023-09-08 13:00:43 +0100

    initite middlewares

--- index.ts
+++ index.ts
@@ -1,17 +1,28 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import router from "./app/router";
-import helmet from "helmet";
+import cors from "cors"
 import hpp from "hpp";
 
-dotenv.config();
-const app: Express = express();
-app.use(bodyParser.json());
+import router from "./app/router";
 
+dotenv.config();
 const port = process.env.PORT || 3000;
+
+const app: Express = express();
+
+/** App configuration */
+app.use(bodyParser.json());
+app.use(cors({
+  credentials: true,
+  origin: "*"
+}));
+app.use(hpp());
+
 router(app);
 
 app.listen(port || 3000, () => {
   return console.log("Server :: running on " + port);
 });
+
+export default app;
