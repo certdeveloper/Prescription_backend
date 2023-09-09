3b7ba2b7ad48fd540c0815a61bc4840eb5768c38
certlegendary <whitestar0713@gmail.com>
2023-09-09 23:19:05 +0100

    Prescription History management apis

--- app/models/Prescriptions.ts
+++ app/models/Prescriptions.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const PrescriptionSchema = new Schema({
-  useId: {
+  userId: {
     type: ObjectId,
     requried: true,
   },
--- app/router.ts
+++ app/router.ts
@@ -5,6 +5,7 @@ import authControllers from "./controllers/auth.controllers";
 import storage from "./middleware/storage.middleware";
 import authMiddleware from "./middleware/auth.middleware";
 import profileControllers from "./controllers/profile.controllers";
+import prescriptionsControllers from "./controllers/prescriptions.controllers";
 
 export default (app: Application) => {
   const router = Router();
@@ -18,7 +19,14 @@ export default (app: Application) => {
   
   /** Routes for Letter header image management */
   router.post("/letterHeadImage/add", authMiddleware.authenticate, storage('letter_head_images').single('file'), profileControllers.addLeadHeadImage)
-  router.get('/letterHeadImage', profileControllers.getletterHeadImage)
+  router.get('/letterHeadImage', profileControllers.getLetterHeadImage)
+  router.post('/letterHeadImage/update', authMiddleware.authenticate, profileControllers.updateLetterHeadImage)
   
+  /** Routes for Prescription history */
+  router.post("/prescriptions/add", authMiddleware.authenticate, storage('prescriptions_pdf').single('file'), prescriptionsControllers.add)
+  router.get('/prescription', prescriptionsControllers.get)
+  router.get('/prescriptions/all', prescriptionsControllers.getAll)
+  router.delete('/prescription/delete', prescriptionsControllers.deleteOne)
+
   app.use("/", router);
 }
